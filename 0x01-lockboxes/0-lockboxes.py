a539f3abcbd381e11deec2a3de6d5cd5e2e81a60
Frankie Vexx <franblog7@gmail.com>
2023-07-04 13:23:07 +0300

    interview test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,15 +13,15 @@ def canUnlockAll(boxes):
     num_of_boxes = len(boxes)
     unlocked_boxes = [False] * num_of_boxes
     unlocked_boxes[0] = True
-    
+
     stack = [0]
-    
+
     while stack:
         box = stack.pop()
-        
+
         for key in boxes[box]:
             if key < num_of_boxes and not unlocked_boxes[key]:
                 unlocked_boxes[key] = True
                 stack.append(key)
-                
+
     return all(unlocked_boxes)
