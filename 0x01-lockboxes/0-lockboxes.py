9259cdd4a856a1ad3ace9002d39cb93eff28c5fb
Frankie Vexx <franblog7@gmail.com>
2023-07-06 17:20:40 +0300

    correcting lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,24 +2,26 @@
 '''returns true if lockboxes can all be opened and false if not'''
 
 
-def join(list1, list2):
-    results = []
-    for numb in list2:
-        results += list1[numb]
-    return results
+def unlocker(box, openboxes, boxes):
+    """Unlock the boxes"""
+    if len(boxes) == len(openboxes):
+        return openboxes
+    for value in box:
+        if value >= len(boxes):
+            continue
+        if value not in openboxes:
+            openboxes.add(value)
+            unlocker(boxes[value], openboxes, boxes)
+    return openboxes
 
 
 def canUnlockAll(boxes):
-    '''returns true of locboxes can all be opened and false if not'''
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    new = True
-    while new:
-        new = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                new = True
-        return len(total) == len(boxes)
-    
+    """Check if unlocked"""
+    openboxes = {0}
+    if len(boxes) == 1:
+        return True
+    unlocked = unlocker(boxes[0], openboxes, boxes)
+    if len(unlocked) == len(boxes):
+        return True
+    else:
+        return False
